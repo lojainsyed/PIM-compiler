240e8cf8c4799b0a2f1bb65a410c4692664020bd
lojainsyed <lojainsyed@arizona.edu>
2026-04-24 15:06:25 -0700

    Add files via upload

---
+++ Parser_updated/tests/pass_2_expressions.c
@@ -0,0 +1,7 @@
+int a = 8;
+int b = 4;
+int c = 2;
+
+int result = a + b * c - (3 + 1);
+
+print result;
---
+++ Parser_updated/tests/pass_3_matrix.c
@@ -0,0 +1,11 @@
+M[0][0] = 10;
+M[0][1] = 5;
+
+int a = M[0][0];
+int b = M[0][1];
+
+int sum = a + b;
+
+M[0][2] = sum;
+
+print sum;
